69bd07bd068545ef6a8f480d323631e057a31001
BUNNYOI <ndnhana24103@cusc.ctu.edu.vn>
2026-08-06 21:25:00 +0700

    feat(deck): implement update deck endpoint

---
+++ backend/OctoFlashcardStudy.API/Contracts/Decks/UpdateDeckRequest.cs
@@ -0,0 +1,11 @@
+﻿using OctoFlashcardStudy.API.Domain.Enums;
+
+namespace OctoFlashcardStudy.API.Contracts.Decks
+{
+    public class UpdateDeckRequest
+    {
+        public string Name { get; init; } = string.Empty;
+        public string? Description { get; init; }
+        public DeckVisibility Visibility { get; init; }  
+    }
+}
